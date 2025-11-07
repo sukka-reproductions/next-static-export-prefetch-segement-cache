8d21da739675a9848048e9ada1bd46c152149407
SukkaW <isukkaw@gmail.com>
2025-11-08 00:40:33 +0800

    Demo

--- README.md
+++ README.md
@@ -1,36 +1,16 @@
-This is a [Next.js](https://nextjs.org) project bootstrapped with [`create-next-app`](https://nextjs.org/docs/app/api-reference/cli/create-next-app).
+# next-static-export-prefetch-segement-cache
 
-## Getting Started
+A demo for https://github.com/vercel/next.js/pull/85903
 
-First, run the development server:
+## How to run
 
 ```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
+pnpm install
+pnpm run build
+# Python http.server doesn't have range request support by default, suitable for this demo
+python3 -m http.server -d out
 ```
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+## Screenshot
 
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
-
-This project uses [`next/font`](https://nextjs.org/docs/app/building-your-application/optimizing/fonts) to automatically optimize and load [Geist](https://vercel.com/font), a new font family for Vercel.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/app/building-your-application/deploying) for more details.
+![screenshot](./screenshot.png)
---
+++ src/app/c/page.tsx
@@ -0,0 +1,40 @@
+import Link from "next/link";
+
+export default function PageA() {
+  return (
+    <>
+      <h1>Very Huge Page</h1>
+      <Link href="/">Go to Home</Link>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a lacinia dolor. Mauris vitae feugiat augue. Quisque sit amet nisl quam. Quisque dapibus finibus enim non laoreet. Morbi fringilla mi finibus ex ultricies blandit. Duis augue ligula, molestie non est et, vehicula euismod nunc. Aenean aliquam eget ante nec pulvinar. Aliquam pellentesque tincidunt turpis, vitae tempus neque laoreet non. Pellentesque gravida dolor magna, a dignissim diam pellentesque eu. Fusce aliquet laoreet mauris volutpat lobortis. Integer non diam mi.
+      </p>
+      <p>
+        Nunc porttitor nisl sed tellus dignissim, vel ultrices velit dapibus. Proin consequat euismod orci, sit amet fringilla justo varius eget. Fusce nec odio vitae sapien maximus lobortis. Nam ultrices mattis fermentum. Nunc vulputate sapien in imperdiet tempor. Praesent tempor fringilla mi in aliquet. Proin rutrum lacus eu tellus scelerisque pharetra. Fusce dignissim eleifend dapibus. Etiam dignissim, erat et posuere imperdiet, tellus ex porttitor ante, convallis gravida augue nisl quis ex. Quisque auctor urna eget tellus finibus, at ornare dui malesuada.
+      </p>
+      <p>
+        Pellentesque tincidunt vulputate turpis, ut pulvinar elit faucibus aliquet. Maecenas ullamcorper enim et porttitor fermentum. Fusce sed nibh velit. Nulla odio nisl, varius quis ex eget, sodales elementum nibh. Donec vehicula dictum nulla at imperdiet. Proin et lorem sed tellus accumsan porttitor ut in neque. Vivamus a fermentum odio, in tincidunt mauris. Praesent eget fermentum sem. Cras consequat blandit magna pharetra volutpat. Morbi mattis orci vitae ultrices pulvinar.
+      </p>
+      <p>
+        Etiam vel felis volutpat tellus tincidunt hendrerit. Etiam vitae consectetur justo. Integer ultrices diam sit amet dignissim molestie. Proin ante risus, luctus a eros rutrum, eleifend faucibus erat. Mauris massa nisi, mollis non tincidunt non, commodo vitae lorem. Quisque at tincidunt neque. Nulla ultrices accumsan sollicitudin.
+      </p>
+      <p>
+        Morbi vitae lectus orci. Vivamus eu tortor diam. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nullam porta ligula elit, non auctor tellus blandit non. Praesent vel sollicitudin lacus, in vestibulum nisi. Ut eu bibendum tellus. Morbi et felis arcu.
+      </p>
+      <p>
+        Nulla vehicula, magna vel facilisis mattis, nibh eros semper velit, a commodo sapien mi et est. Cras ipsum quam, facilisis in condimentum eu, vestibulum consequat quam. Nullam turpis odio, luctus efficitur rhoncus id, iaculis eu mi. Ut vel ipsum non neque maximus dapibus ut ut mi. Aenean non efficitur leo, a hendrerit nibh. Nam eu nunc ac arcu posuere euismod id eu lorem. Pellentesque consectetur tincidunt efficitur. Pellentesque bibendum eu lacus at finibus. Donec laoreet, orci vitae aliquet vestibulum, ex erat condimentum nisi, vitae scelerisque ipsum metus et urna. Curabitur dictum gravida maximus. Nullam vitae lacinia augue. Morbi at mattis quam, ut varius augue. Nunc fermentum dictum nunc, nec mollis metus egestas non. Vestibulum fringilla, ex eget tincidunt accumsan, augue nulla laoreet est, id fermentum massa ex sed leo. Integer euismod massa sit amet turpis interdum fringilla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+      </p>
+      <p>
+        Praesent sed feugiat orci. Nunc velit neque, egestas quis iaculis non, consectetur eu neque. Vestibulum diam elit, aliquam sed sem ut, sagittis sagittis augue. Integer molestie mi vel augue convallis, ut interdum ante imperdiet. Phasellus tristique sed magna non volutpat. Fusce posuere sodales nisl, id suscipit erat euismod quis. Donec at libero semper, ultricies orci eu, ornare felis. Cras posuere ultricies vehicula.
+      </p>
+      <p>
+        Maecenas ac ultrices sem, vel dapibus metus. Cras vitae posuere libero, in ullamcorper libero. Morbi blandit felis et velit finibus maximus. Etiam ac nunc non felis rutrum dictum. Morbi tellus erat, blandit eget convallis ac, scelerisque aliquam nunc. Proin erat tortor, rutrum vitae ex et, convallis varius urna. Aliquam vulputate id dui quis rhoncus. Morbi rutrum felis et lorem auctor placerat. Nam rhoncus purus eu nunc rutrum elementum. Mauris tincidunt non tellus sit amet consectetur.
+      </p>
+      <p>
+        Aliquam turpis enim, eleifend in vulputate ac, mattis vitae odio. Proin luctus, felis vel ultrices efficitur, nulla tellus feugiat enim, in convallis risus ipsum eget sapien. Quisque eu sem mattis, sollicitudin purus a, commodo ligula. Aliquam tincidunt ornare facilisis. Integer iaculis purus ut molestie suscipit. Integer egestas feugiat ipsum, sed sagittis neque dignissim eu. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum efficitur odio ac enim luctus tincidunt. In vestibulum efficitur magna eget aliquam. Nunc turpis justo, varius id ultrices sit amet, pharetra ut nisi.
+      </p>
+      <p>
+        Proin rhoncus, velit et porta suscipit, metus felis iaculis lorem, sit amet varius purus tortor a odio. Nunc eget suscipit justo, quis faucibus nisl. Nunc imperdiet magna vel risus sodales condimentum. Quisque ac augue egestas, aliquam ligula placerat, bibendum nisi. Ut velit.
+      </p>
+    </>
+  )
+}
